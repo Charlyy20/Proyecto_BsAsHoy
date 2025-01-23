f5017f1cd23d906e5624622a0d77cc1abbfb40d9
Carlos <carlosl200400@gmail.com>
2025-01-23 17:53:05 -0300

    Plantilla funcionando

--- proyect/core/urls.py
+++ proyect/core/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'core'
+
 urlpatterns = [
-    path("", views.home),
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("post/", views.post, name="post"),
+    path("contact/", views.contact, name="contact"),
+    path("category/", views.category, name="category"),
+    
 ]
--- proyect/core/views.py
+++ proyect/core/views.py
@@ -5,3 +5,14 @@ from . import models
 def home(request):
     return render(request, "core/index.html")
 
+def about(request):
+    return render(request,"core/about.html")
+
+def post(request):
+    return render(request,"core/single-post.html")
+
+def contact(request):
+    return render(request,"core/contact.html")
+
+def category(request):
+    return render(request,"core/category.html")

ef9e14c7f39e8ec9751becac2154cf513cf218cd
Agustín Mattar <agustinmattar1203@gmail.com>
2025-03-24 20:24:19 -0300

    CAMBIOS HTML Y CSS
    -Footer con nuevo color y terminado
    -Cambios página "contacto" (falta entrelazar formulario con email)
    -Links de redes sociales

--- proyect/core/urls.py
+++ proyect/core/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 app_name = 'core'
 
 urlpatterns = [
+    # Otras URLs...
+    path('noticia/<slug:slug>/', views.detalle_noticia, name='detalle_noticia'),
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
     path("post/", views.post, name="post"),
@@ -11,3 +14,4 @@ urlpatterns = [
     path("category/", views.category, name="category"),
     
 ]
+
--- proyect/core/views.py
+++ proyect/core/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
 from . import models
+from django.shortcuts import render, get_object_or_404
+from .models import Noticia
+
+def detalle_noticia(request, slug):
+    # Obtener la noticia basada en el slug (o lanzar un error 404 si no existe)
+    noticia = get_object_or_404(Noticia, slug=slug)
+    
+    # Pasar la noticia al template
+    return render(request, 'core/single-post.html', {'noticia': noticia})
 
 def home(request):
     return render(request, "core/index.html")
@@ -16,3 +24,6 @@ def contact(request):
 
 def category(request):
     return render(request,"core/category.html")
+
+
+
